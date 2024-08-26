5caddf1e8e4dc5e1c3df40be1900c9a3d67a1743
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-08-26 14:37:48 +0200

    Create basic mathematical functions

---
+++ javascript.js
@@ -0,0 +1,21 @@
+//Basic functions
+
+function add(a, b) {
+  sum = a + b;
+  return sum;
+}
+
+function subtract(a, b) {
+  diff = a - b;
+  return diff;
+}
+
+function multiply(a, b) {
+  prod = a * b;
+  return prod;
+}
+
+function divide(a, b) {
+  quot = a / b;
+  return quot;
+}
