9a3a04eca9224b83f0f3fb0ca270e7219357e658
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-08-26 14:52:30 +0200

    Create operate functions

--- javascript.js
+++ javascript.js
@@ -19,3 +19,26 @@ function divide(a, b) {
   quot = a / b;
   return quot;
 }
+
+//Operation variables
+
+let op1; //1st operand
+let op2; //2nd operand
+let operator; //operator
+
+//Basic operation
+
+function operate(op1, op2, operator) {
+  switch (operator) {
+    case "+":
+      return add(op1, op2);
+    case "-":
+      return subtract(op1, op2);
+    case "*":
+      return multiply(op1, op2);
+    case "/":
+      return divide(op1, op2);
+    default:
+      return "error";
+  }
+}
