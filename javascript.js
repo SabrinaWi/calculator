a28df3ed001f62c1c2d98f1b6d3f346c2ee5e1f3
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-09-04 12:07:39 +0200

    Store number and operator button input

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ function divide(a, b) {
   return quot;
 }
 
-//Operation variables
+//Variables for storing input
 
 let num1 = []; //1st operand, use array to store multiple digits
 let num2 = []; //2nd operand, use array to store multiple digits
@@ -37,6 +37,77 @@ function numberFromInput(input) {
 }
 
 //Object for storing current variables
+
+//Select buttons
+
+const btns = document.querySelectorAll("button");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    deactivateBtns(btns); //remove visual active state from previously used button
+    activateBtn(btn); //mark current button visually as active
+  });
+});
+
+//show visually when a button is active/has been clicked
+function activateBtn(button) {
+  button.classList.add("active");
+}
+
+//remove (visual) active state from previously active buttons
+function deactivateBtns(buttons) {
+  buttons.forEach((button) => button.classList.remove("active"));
+}
+
+//number buttons
+
+const numBtns = document.querySelectorAll(".num-btn");
+
+numBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", (event) => {
+    storeNumBtnInput(numBtn);
+  });
+});
+
+function storeNumBtnInput(numBtn) {
+  if (!op) {
+    //stores new input in num1 array as long as there there has been no operator input
+    num1.push(numBtn.name.slice(-1));
+    return num1;
+  } else {
+    //otherwise stores new input in num2 array
+    num2.push(numBtn.name.slice(-1));
+    return num2;
+  }
+}
+
+//operator buttons
+
+const opBtns = document.querySelectorAll(".op-btn");
+
+opBtns.forEach((opBtn) => {
+  opBtn.addEventListener("click", (event) => {
+    storeOpBtnInput(opBtn);
+  });
+});
+
+function storeOpBtnInput(opBtn) {
+  if (num1) {
+    switch (opBtn.name) {
+      //stores new input in op as long as there there is already a num1
+      case "btn-add":
+        return (op = "+");
+      case "btn-sub":
+        return (op = "-");
+      case "btn-mul":
+        return (op = "*");
+      case "btn-div":
+        return (op = "/");
+      default:
+        return "error";
+    }
+  }
+}
 
 //Basic operation
 
